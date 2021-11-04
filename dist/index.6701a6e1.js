12cb746a4f99ae5c09eb13e51a151ae07280a0b8
Michele <michelemascia1996@gmail.com>
2021-11-04 18:56:55 +0100

    Update index.6701a6e1.js

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -22756,7 +22756,7 @@ class MainView extends _reactDefault.default.Component {
         };
     }
     componentDidMount() {
-        _axiosDefault.default.get('https://myflix-heroku.herokuapp.com/movies').then((response)=>{
+        _axiosDefault.default.get('https://raftelapi.herokuapp.com/movies').then((response)=>{
             this.setState({
                 movies: response.data
             });
