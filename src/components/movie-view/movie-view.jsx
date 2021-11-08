000e6e434a50f42b16e1ade5c991fd5fe566007b
Michele <michelemascia1996@gmail.com>
2021-11-08 18:12:06 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -12,11 +12,11 @@ export class MovieView extends React.Component {
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+          <span className="value">{movie.title}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
+          <span className="value">{movie.description}</span>
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
