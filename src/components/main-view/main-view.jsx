00c179965bfc4857a7eb4890bf76d0da4a155933
Michele <michelemascia1996@gmail.com>
2021-11-08 18:12:08 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
-    constructor(){
-        super();
-        this.state = {
-          movies: [
-            { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: '...'},
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: '...'},
-            { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: '...'}
-          ],
-          selectedMovie: null
-        }
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null
     }
+  }
+  
+  componentDidMount(){
+    axios.get('https://raftelapi.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
     setSelectedMovie(newSelectedMovie) {
       this.setState({
