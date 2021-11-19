37491c036c84e550f06b37a6c27d3e5052867058
Michele <michelemascia1996@gmail.com>
2021-11-19 01:30:50 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+//import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 
 
 export class MainView extends React.Component {
@@ -58,10 +60,10 @@ export class MainView extends React.Component {
       );
     }*/
 
-    render() {
+    /*render() {
       const { movies, selectedMovie, user } = this.state;
   
-      /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+      // If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView
       if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
   
       // Before the movies have been loaded
@@ -69,15 +71,62 @@ export class MainView extends React.Component {
   
       return (
         <div className="main-view">
-          {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+          {//If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned}
           {selectedMovie
-            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            ? 
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
             : movies.map(movie => (
               <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
            ))
           }
         </div>
       );
+    }*/
+    render() {
+      const { movies, selectedMovie, user } = this.state;
+  
+      /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details 
+      are *passed as a prop to the LoginView*/
+      if(!user) return  <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+  
+      // Before the movies have been loaded
+      if (movies.length === 0) return <div className="main-view" />;
+  
+      return (
+        <div className="main-view">
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand color="white" href="#home">MyFlix APP</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Profile</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <Row className="main-view justify-content-md-center">
+          {selectedMovie
+            ? (
+              <Col>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            )
+            : movies.map((movie) => (
+              <Col md={3} key={movie._id}>
+                <MovieCard
+                  movie={movie}
+                  onMovieClick={(newSelectedMovie) => {
+                    this.setSelectedMovie(newSelectedMovie);
+                  }}
+                />
+              </Col>
+            ))        
+          }
+          </Row>
+        </div>
+      );    
     }
 }
 
