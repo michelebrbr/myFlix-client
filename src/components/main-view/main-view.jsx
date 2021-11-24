b453c578ebd52064b39093be319b2c7d56b3c21f
Michele <michelemascia1996@gmail.com>
2021-11-24 22:00:21 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -32,7 +32,7 @@ export class MainView extends React.Component {
     });
   }
   
-  componentDidMount(){
+  /*componentDidMount(){
     axios.get('https://raftelapi.herokuapp.com/movies')
       .then(response => {
         this.setState({
@@ -42,6 +42,16 @@ export class MainView extends React.Component {
       .catch(error => {
         console.log(error);
       });
+  }*/
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
+    }
   }
 
     setSelectedMovie(newSelectedMovie) {
@@ -67,12 +77,21 @@ export class MainView extends React.Component {
       this.getMovies(authData.token);
     }
 
+    onLoggedOut() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      this.setState({
+        user: null
+      });
+    }
+
     render() {
       const { movies, selectedMovie, user } = this.state;
   
       /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details 
       are *passed as a prop to the LoginView*/
       if(!user) return  <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+      
   
       // Before the movies have been loaded
       if (movies.length === 0) return <div className="main-view" />;
@@ -81,6 +100,7 @@ export class MainView extends React.Component {
         <div className="main-view">
           <Navbar bg="dark" expand="lg">
             <Container>
+            <button onClick={() => { this.onLoggedOut() }}>Logout</button>
               <Navbar.Brand href="#home" style={{ color:"#ffffff"}}>MyFlix APP</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
@@ -105,6 +125,7 @@ export class MainView extends React.Component {
                   onMovieClick={(newSelectedMovie) => {
                     this.setSelectedMovie(newSelectedMovie);
                   }}
+                  
                 />
               </Col>
             ))        
