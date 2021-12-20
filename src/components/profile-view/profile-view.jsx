f12c96feacad6f85ce846957edd1d64127ba9c1b
Michele <michelemascia1996@gmail.com>
2021-12-20 18:03:04 +0100

    Update profile-view.jsx
    created profile-view

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class ProfileView extends React.Component {
+
+  render() {
+    console.log(this.props);
+    const { userObject } = this.props;
+    return(
+      <div>
+        <p>username: {userObject.username}</p>
+        <p>email: {userObject.email}</p>
+        <p>birthday: {userObject.birthday}</p>
+      </div>
+    )
+    
+  }
+}
