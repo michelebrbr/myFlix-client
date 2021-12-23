227c554a53ffbbba7aa8d0b7f83c224ad30d486b
Michele <michelemascia1996@gmail.com>
2021-12-23 17:47:35 +0100

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -4,13 +4,18 @@ import { Link } from "react-router-dom";
 import './director-view.scss'
 
 export class DirectorView extends React.Component {
+
     render(){
-        console.log('dw props:',this.props);
+        const { director } = this.props;
+        console.log(director);
+
         return(
             <div>
-                <p>Director view:</p>
+                <h1>Director view:</h1>
+                <div>{director.name}</div>
+                
                 <Link to={`/`} className="mr-2">
-                    <button>Home</button>
+                    <button>go back</button>
                 </Link>
             </div>
         )
