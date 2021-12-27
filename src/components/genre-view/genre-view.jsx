74a307c45a2fecf42471fd7d97eaf7a36d98e30f
Michele <michelemascia1996@gmail.com>
2021-12-27 13:16:24 +0100

    Update genre-view.jsx

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -5,21 +5,23 @@ import './genre-view.scss'
 
 export class GenreView extends React.Component {
     render(){
-        const { genre } = this.props
-        console.log('gw props:',this.props);
+        const { genreObject } = this.props;
+        console.log(genreObject);
+
+
         return(
             <div className="genre-view" style={{ width: '25%', margin: 'auto' }}>
-            <div className="genre-name">
-                <span className="label">Name: <br></br></span>
-                <span className="value">{genre.name}</span>
-            </div>
-            <div className="genre-description">
-                <span className="label"><br></br>Description: <br></br></span>
-                <span className="value">{genre.description}</span>
-            </div>
-            <Link to={`/`} className="mr-2">
-                <button>go back</button>
-            </Link>
+                <div className="genre-name">
+                    <span className="label">Name: <br></br></span>
+                    <span className="value">{genreObject.name}</span>
+                </div>
+                <div className="genre-description">
+                    <span className="label"><br></br>Description: <br></br></span>
+                    <span className="value">{genreObject.description}</span>
+                </div>
+                <Link to={`/`} className="mr-2">
+                    <button>go back</button>
+                </Link>
         </div>
         )
     }
