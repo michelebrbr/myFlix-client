8688f35da17f3682fdce2017b6bdf558a155c2bc
Michele <michelemascia1996@gmail.com>
2021-12-27 15:35:19 +0100

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,43 +1,71 @@
-/*import React, { useState } from 'react';
-import { Button, Form, Container, Col, Row, CardGroup, Card } from 'react-bootstrap';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import axios from 'axios'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {  BrowserRouter as Router } from 'react-router-dom';
+import './registration-view.scss';
 
-export function RegistrationView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export function RegistrationView() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    // Send a request to the server for authentication, then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
+    console.log(username, password, email, birthday);
+    axios.post('https://raftelapi.herokuapp.com/users', {
+      username: username,
+      password: password,
+      email: email,
+      birthday: birthday
+    })
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+    })
+    .catch(e => {
+      console.log('error registering the user')
+    });
   };
 
-
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Form>
-            <CardGroup>
-              <Card>
-                <Card.Header>Welcome! Please Register</Card.Header>
-                <Form.Group controlId="formUsername">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group controlId="formPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                  Submit
-                </Button>
-              </Card>
-            </CardGroup>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <div className="registration" style={{ width: '30%', margin: 'auto' }}>
+    <Router>   
+    <Form>
+    <h1 className="form-title">Register</h1>
+      <Form.Group controlId="registration-Username">
+       <Form.Label>Username:</Form.Label>
+       <Form.Control className="username" value={username} type="text" placeholder="Enter a new username" onChange={e => setUsername(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group controlId="registration-Password">
+       <Form.Label>Password:</Form.Label>
+       <Form.Control className="password" value={password} type="text" placeholder="Set a password" onChange={e => setPassword(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group controlId="registration-Email">
+       <Form.Label>Email:</Form.Label>
+       <Form.Control className="email" value={email} type="email" placeholder="Enter an email" onChange={e => setEmail(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group controlId="registration-Birthday">
+       <Form.Label>Birthday:</Form.Label>
+       <Form.Control className="birthday" value={birthday} type="date" placeholder="Enter your birthday" onChange={e => setBirthday(e.target.value)}></Form.Control>
+      </Form.Group>
+      <div className="buttons-registration">
+      <Button variant="success link" className="registerBtn" type="submit" onClick={handleSubmit}>Register </Button>
+      </div>
+    </Form>
+    </Router>
+  </div>
   );
-}*/
+}
+
+RegistrationView.propTypes = {
+  register: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    birthday: PropTypes.string.isRequired,
+  }),
+};
