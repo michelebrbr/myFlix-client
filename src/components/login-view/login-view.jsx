480aac4349bf1b5f71a83f2c3cc22d863cb47eaf
Michele <michelemascia1996@gmail.com>
2021-12-29 15:59:06 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
 import { Button, Form, Container, Col, Row, CardGroup, Card } from 'react-bootstrap';
 import axios from 'axios';
-import {  BrowserRouter as Router } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
-  //mockup login
-
-  /*const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    // Send a request to the server for authentication, then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
-  };*/
   const handleSubmit = (e) => {
     e.preventDefault();
     // Send a request to the server for authentication
@@ -33,38 +23,35 @@ export function LoginView(props) {
     });
   };
 
-
   return (
     <Container style={{ width: '30%', margin: 'auto' }}>
-      <Row>
-        <Col>
-          <Form>
-            <CardGroup>
-              <Card >
-                <Card.Header>Welcome! Please Login</Card.Header>
-                <Form.Group controlId="formUsername">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-                </Form.Group>
+        <Row>
+          <Col>
+            <Form>
+              <CardGroup>
+                <Card >
+                  <Card.Header>Welcome! Please Login</Card.Header>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                  </Form.Group>
 
-                <Form.Group controlId="formPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                  Submit
-                </Button>
-                <Router>
-                <Card.Header>Not registered yet?</Card.Header>
-                  <Link to={`/register`} className="mr-2">
-                    <button>Register</button><br></br>
-                   </Link>
-                </Router>
-              </Card>
-            </CardGroup>
-          </Form>
-        </Col>
-      </Row>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Submit
+                  </Button>
+                  <Card.Header>Not registered yet?</Card.Header>
+                </Card>
+              </CardGroup>
+            </Form>
+            <Link to="/register" className="mr-2">
+              <button>Register</button>
+            </Link>
+          </Col>
+        </Row> 
     </Container>
   );
 }
