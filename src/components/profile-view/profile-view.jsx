9250c8f77f12c8e814c3b49a71c386356f9981bf
Michele <michelemascia1996@gmail.com>
2021-12-29 16:52:29 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+
+
 
 export class ProfileView extends React.Component {
   componentDidMount() {
@@ -7,6 +12,26 @@ export class ProfileView extends React.Component {
     console.log(this.props);
     console.log(this.state);
 
+  }
+
+  deleteAccount() {
+
+    const answer = window.confirm("Are you sure you want to delete your account?");
+    if (answer) {
+      const token = localStorage.getItem("token");
+      const user = localStorage.getItem("user");
+        axios.delete( `https://raftelapi.herokuapp.com/users/${user}`,
+          { headers: { Authorization: `Bearer ${token}` } }
+          )
+          .then(() => {
+            alert(user + " has been deleted.");
+            localStorage.removeItem('user');
+            localStorage.removeItem('token');
+            window.location.pathname = "/";
+          })
+          .catch(function (error) {
+              console.log(error);
+          })};        
   }
 
   
@@ -19,7 +44,11 @@ export class ProfileView extends React.Component {
         <p>username: {userObject.username}</p>
         <p>email: {userObject.email}</p>
         <p>birthday: {userObject.birthday}</p>
-        <p>favorites: {userObject.favmovies}</p>
+        <p>favorites: {userObject.favoriteMovies}</p>
+        <Link to={`/`} className="mr-2">
+          <button>go back</button>
+        </Link>
+        <Button size="md" variant="outline-danger" type="submit" ml="4" onClick={() => this.deleteAccount()} >Delete Account</Button>
       </div>
     )
     
