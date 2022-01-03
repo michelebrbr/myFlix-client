547ecc5bb54e116962ad086a7486551b6f0ffa69
Michele <michelemascia1996@gmail.com>
2022-01-03 18:23:39 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 
 
 export class ProfileView extends React.Component {
   componentDidMount() {
-    const { userObject } = this.props;
-    this.setState({ userObject });
-    console.log(this.props);
-    console.log(this.state);
+    //const { userObject, movie } = this.props;
+    //this.setState({ userObject });
+    //console.log(this.props);
+    //console.log(this.state);
 
   }
 
+  removeFavorite = (movie) => {
+    const user = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+    console.log(this.props)
+    axios.delete(`https://raftelapi.herokuapp.com/users/${user}/movies/${movie._id}`, 
+    { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        console.log(response);
+        alert("Movie was removed");
+        this.componentDidMount();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+ 
   deleteAccount() {
 
     const answer = window.confirm("Are you sure you want to delete your account?");
@@ -27,6 +43,7 @@ export class ProfileView extends React.Component {
             alert(user + " has been deleted.");
             localStorage.removeItem('user');
             localStorage.removeItem('token');
+            localStorage.removeItem('userObject');
             window.location.pathname = "/";
           })
           .catch(function (error) {
@@ -34,22 +51,40 @@ export class ProfileView extends React.Component {
           })};        
   }
 
-  
-  
   render() {
     
-    const { userObject } = this.props;
-    return(
-      <div>
+    const { userObject, favMovies } = this.props;
+    if (favMovies.length === 0){
+      return <div style={{ width: '30%', margin: 'auto' }}>
         <p>username: {userObject.username}</p>
         <p>email: {userObject.email}</p>
         <p>birthday: {userObject.birthday}</p>
-        <p>favorites: {userObject.favoriteMovies}</p>
-        <Link to={`/`} className="mr-2">
-          <button>go back</button>
-        </Link>
-        <Button size="md" variant="outline-danger" type="submit" ml="4" onClick={() => this.deleteAccount()} >Delete Account</Button>
+        <p>favorites: There are no favorite movies</p>
       </div>
+    }
+
+    return(
+      <div style={{ width: '30%', margin: 'auto' }}>
+        <p>username: {userObject.username}</p>
+        <p>email: {userObject.email}</p>
+        <p>birthday: {userObject.birthday}</p>
+        <p>favorites:</p>
+
+    
+
+          {favMovies.map(movie => ( 
+              <Card key={movie._id}>
+                <Card.Img variant="top" src={movie.ImagePath} />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Card.Text>{movie.description}</Card.Text>
+                  <Button onClick={() => this.removeFavorite(movie)}>Remove from favorites</Button>
+
+                </Card.Body>
+              </Card> ))}
+          <Button size="md" variant="outline-danger" type="submit" ml="4" onClick={() => this.deleteAccount()} >Delete Account</Button>
+        
+        </div>
     )
     
   }
