1cec11e7286972a615a403570fdf5a5d55be3456
Michele <michelemascia1996@gmail.com>
2022-01-10 22:32:51 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
 
@@ -9,8 +10,8 @@ export class MovieCard extends React.Component {
     const { movie, onMovieClick, addFavorite } = this.props;
 
     return (
-      <Card border="dark">
-        <Card.Img variant="top" src={movie.imagePath}/>
+      <Card border="dark" className="mb-2">
+        <Card.Img variant="top" src={movie.imagePath} className="cardImg"/>
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Button style={{ color:"#808080"}} onClick={() => onMovieClick(movie)} variant="link">View details</Button>
